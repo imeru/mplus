05a764a25908afe00fdd3b00095a4293dfc742b2
Hyunwoo Lim <ecosoop@gmail.com>
2014-02-11 23:17:29 -0700

    Copy folders from original data set and then run energyplus respectively.
    Usage:
    $ python mplus.py
    or
    $ time python mplus.py
    
    You should care about four folders after running this program.
    FIXME: Remove it or not, you should decide it in the next step.

--- mplus.py
+++ mplus.py
@@ -1,6 +1,7 @@
 from multiprocessing import Pool, cpu_count
 from subprocess import call
 import os
+import shutil
 
 def run_energyplus(path):
     current_dir = os.getcwd()
@@ -8,8 +9,16 @@ def run_energyplus(path):
     call(["energyplus"])
     os.chdir(current_dir)
 
+def copy_folder(origin, destination):
+    shutil.copytree(origin, destination)
+
 if __name__ == '__main__':
+    ORIGIN_DATA = "data"
+    pathes = []
+    for i in range(4):
+        destination = ORIGIN_DATA + "_"  + str(i)
+        copy_folder(ORIGIN_DATA, destination)
+	pathes.append(destination)
     CORES = cpu_count()
-    pathes = ["data", "data2"]
     p = Pool(CORES)
     print p.map(run_energyplus, pathes)
