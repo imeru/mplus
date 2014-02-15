7ea125bb620d66ec6d50171423e12dd6e6b4e4d3
Sanghee Kim <sangheestyle@gmail.com>
2014-02-14 18:22:15 -0700

    Using regular expression to find int or float
    Please study about moduel re and regular expression grammer.
    See,
    http://goo.gl/sP9gGU
    http://docs.python.org/2/library/re.html
    http://goo.gl/8HStFg

--- example/test1.py
+++ example/test1.py
@@ -1,5 +1,7 @@
+import re
+
 origin = open('idf_sample/demo.idf', 'r')
 for line in origin:
-    line = line.replace("U-Factor", "@@U-FACTOR@@")
-    print line
+    if "U-Factor" in line:
+        print re.search(r'[-+]?\d*\.\d+|\d+', line).group()
 origin.close()
