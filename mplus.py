d53384204efd44e6298f5ffeec0f6ea7d9c99ddf
ecosoop@gmail.com <ecosoop@gmail.com>
2014-02-18 23:38:02 -0700

    refactoring running eplus multi function to use easily

--- mplus.py
+++ mplus.py
@@ -3,11 +3,16 @@ from subprocess import call
 import os
 import shutil
 
-def run_energyplus(path):
+def run_eplus(path):
     current_dir = os.getcwd()
     os.chdir(path)
     call(["energyplus"])
     os.chdir(current_dir)
+
+def run_eplus_multi(pathes):
+    p = Pool(cpu_count())
+    result = p.map(run_eplus, pathes)
+    return result
 
 def copy_folder(origin, destination):
     shutil.copytree(origin, destination)
@@ -19,6 +24,4 @@ if __name__ == '__main__':
         destination = ORIGIN_DATA + "_"  + str(i)
         copy_folder(ORIGIN_DATA, destination)
 	pathes.append(destination)
-    CORES = cpu_count()
-    p = Pool(CORES)
-    print p.map(run_energyplus, pathes)
+    run_eplus_multi(pathes)
