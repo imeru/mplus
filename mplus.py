89262ad8ff47de6d7f4ee2c592660920b8efa837
Sanghee Kim <sangheestyle@gmail.com>
2014-02-23 17:20:33 -0700

    fixed: can't execute energyplus due to case sensitive in Linux
    Linux is case sensitive for file and folder naming. So the name
    energyplus should be renamed to EnergyPlus.
    
    FIXME:
    Please test it in Windows environment.

--- mplus.py
+++ mplus.py
@@ -6,7 +6,7 @@ import shutil
 def run_eplus(path):
     current_dir = os.getcwd()
     os.chdir(path)
-    call(["energyplus"])
+    call(["EnergyPlus"])
     os.chdir(current_dir)
 
 def run_eplus_multi(pathes):
