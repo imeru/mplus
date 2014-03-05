bd13fd3c18313a5e13c220fa6b8c725708934efd
Sanghee Kim <sangheestyle@gmail.com>
2014-03-05 00:57:20 -0700

    Accumulate result files into one csv file
    fixed #4

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import shutil
+import csv
 from mplus import run_eplus_multi
 from make_idf import generate_markup_value_pairs, write_idf, copy_files
 from make_idf import prepare_job_folders
@@ -45,3 +46,22 @@ for path in eplus_out_csv_pathes:
     dest_pathes.append(path)
 for orig, dest in zip(eplus_out_csv_pathes, dest_pathes):
     shutil.copyfile(orig, dest)
+
+# gathering csv files into a csv file
+csv_sum = []
+with open(dest_pathes[0]) as csv_file:
+    rows = csv.reader(csv_file, delimiter='|')
+    contents = [row for row in rows]
+    csv_sum.extend(contents[0])
+
+for file in dest_pathes:
+    with open(file) as csv_file:
+        rows = csv.reader(csv_file, delimiter='|')
+        contents = [row for row in rows]
+        csv_sum.extend(contents[1])
+
+csv_sum_file_name = "csv_sum.csv"
+csv_sum_file_path = os.path.join(result_path, csv_sum_file_name)
+with open(csv_sum_file_path, "wb") as file:
+    for row in csv_sum:
+        file.write("%s\n" % row)
